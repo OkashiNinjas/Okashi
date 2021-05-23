84cfcddeec1021a61273c01e221928022f5dc5cb
qasem-moh <2171246@s.aou.edu.jo>
2021-05-23 14:18:54 +0300

    border

--- js/app.js
+++ js/app.js
@@ -36,21 +36,4 @@ document.querySelector('.linkss').scrollIntoView({
 changeLinkStateNav();
 window.addEventListener('scroll', changeLinkStateNav);
 
-/**
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
- */
+
