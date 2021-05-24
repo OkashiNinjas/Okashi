20e8c609a7dde3ac21890e855f208d16bc662ee1
qasem-moh <2171246@s.aou.edu.jo>
2021-05-24 16:17:37 +0300

    day2

--- js/app.js
+++ js/app.js
@@ -36,4 +36,26 @@ document.querySelector('.linkss').scrollIntoView({
 changeLinkStateNav();
 window.addEventListener('scroll', changeLinkStateNav);
 
+// eslint-disable-next-line no-var
+var buttonToTop = document.getElementById('myBtnToTop');
 
+// var navBars = document.getElementById('navBar');
+window.onscroll = function () {
+  scrollFunctionToTop();
+};
+
+function scrollFunctionToTop() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    buttonToTop.style.display = 'block';
+    // navBars.style.backgroundColor= linear-gradient(to right, red,rgb(95, 7, 7), purple, rgb(202, 91, 202));
+  } else {
+    buttonToTop.style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+// eslint-disable-next-line no-unused-vars
+function scrollTopFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
