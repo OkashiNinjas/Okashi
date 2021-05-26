7810d57f06bded56acb4c9b6a3a6147e6b4a6885
Omar Alazaizeh <30804087+Omx302@users.noreply.github.com>
2021-05-26 09:32:15 +0300

    partyOver

--- js/cartCount.js
+++ js/cartCount.js
@@ -1,5 +1,5 @@
 let navBarCart = document.getElementById("cart");
 let local = JSON.parse(localStorage.getItem("products"));
 if (local != null) {
-  cartCounter.textContent = local.length;
+  navBarCart.textContent = local.length;
 }
