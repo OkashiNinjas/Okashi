fb51ffc0916a9b3924b0280c03da97f2c7b9d281
Omar Alazaizeh <omx302@gmail.com>
2021-05-26 13:28:48 +0300

    css update

--- js/products.js
+++ js/products.js
@@ -100,11 +100,90 @@ product.prototype.render = function () {
   productDiv.appendChild(elwrapper);
 };
 
-let test1 = new product("KopiKo", "Candy", 1, 5, "../img/p1.jpg");
-let test2 = new product("Flower Bucket", "Gift", 2, 4, "../img/p3.jpeg");
-let test3 = new product("Air Heads", "Candy", 2, 3, "../img/p2.webp");
-let test4 = new product("Happy Box", "Gift", 20, 5, "../img/p4.jpg");
-
+let p1 = new product(
+  "Colorful bouquet",
+  "Flower",
+  25,
+  5,
+  "https://do5ctr7j643mo.cloudfront.net/wp-content/uploads/2017/02/06095857/Maison-des-Fleurs-Box-of-Flowers.jpg"
+);
+let p2 = new product(
+  "Red roses",
+  "Flower",
+  30,
+  4,
+  "https://maisondesfleurs.com/media/catalog/product/cache/625187da366490656fb834f81d8b48fe/a/w/aw3a0378_1.jpg"
+);
+let p3 = new product(
+  "Red roses",
+  "Flower",
+  15,
+  3,
+  "http://theluxediary.com/wp-content/uploads/2018/01/Maison-Des-Fleurs_Valentine%C2%B4s-Day_Gift-Idea_6.jpg"
+);
+let p4 = new product(
+  "White orchid",
+  "Flower",
+  15,
+  5,
+  "https://nanzandkraft.imgix.net/images/itemVariation/WaterfallOrchidPlant287W-20041714952.jpg"
+);
+let p5 = new product(
+  "Blue orchid",
+  "Flower",
+  15,
+  5,
+  "https://www.avasflowers.net/img/prod_img/avasflowers-lavender-orchid-plant.jpg"
+);
+let p6 = new product(
+  "Candy Cake",
+  "Candy",
+  20,
+  4,
+  "https://fyf.tac-cdn.net/images/products/small/C-100.jpg?auto=webp&quality=60"
+);
+let p7 = new product(
+  "Chocolate bouquet",
+  "Chocolate",
+  20,
+  3,
+  "http://artchocolat.com/wp-content/uploads/2013/08/Chocolate-Bouquets-4-360x360.jpg"
+);
+let p8 = new product(
+  "Candy bouquet",
+  "Candy",
+  20,
+  5,
+  "https://www.en.1800flowers.co.il/images/itempics/bc555_large.jpg"
+);
+let p9 = new product(
+  "Unicorn pinata",
+  "Pinata",
+  35,
+  5,
+  "https://cdn.shopify.com/s/files/1/0254/2030/0362/products/27720-Pink-Unicorn-Pinata_f15246fc-7fce-42d1-bd58-ed42fe81ede2_1024x.jpg?v=1571308984"
+);
+let p10 = new product(
+  "Pikachu pinata",
+  "Pinata",
+  25,
+  4,
+  "https://m.media-amazon.com/images/I/61Osaj0FO1L.jpg"
+);
+let p11 = new product(
+  "Chocolate box",
+  "Chocolate",
+  25,
+  3,
+  "https://www.puyricard.fr/140-large_default/square-box-of-chocolates-300-g.jpg"
+);
+let p12 = new product(
+  "Candy Box",
+  "Candy",
+  20,
+  5,
+  "https://cdn03.ciceksepeti.com/cicek/kc52097-1/XL/turuncu-ruya-seker-sepeti-kc52097-1-1.jpg"
+);
 function addProductToCart(index) {
   for (let i = 0; i < productsArr.length; i++) {
     if (i == index) {
